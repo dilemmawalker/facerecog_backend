b935d4cd3a3c64ffe8d8bb43085f61f9c1bad9c0
yash handa <yashhanda500@gmail.com>
2020-12-19 14:06:36 +0530

    update

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-import 
+import express from 'express';
+import bodyparser from 'body-parser';
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('working file!!!');
+});
+
+app.listen(3000,()=>{
+    console.log('running');
+}); 
